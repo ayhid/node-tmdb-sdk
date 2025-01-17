7ca1042fa44693963f943b939dc5cb486079f276
Ayoub Hidri <ayhid@users.noreply.github.com>
2025-01-17 19:53:13 +0100

    fix: refactor endpoint paths and add new types for movie data

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,9 +1,26 @@
 import type {
-  ConfigurationDetailsData,
-  MovieDetailsData,
   SearchMovieData,
+  MovieDetailsData,
+  ConfigurationDetailsData,
   SearchPersonData,
   TvSeriesDetailsData,
+  MovieCreditsData,
+  MovieImagesData,
+  MovieKeywordsData,
+  MovieReleaseDatesData,
+  MovieVideosData,
+  MovieTranslationsData,
+  MovieAlternativeTitlesData,
+  PersonDetailsData,
+  PersonMovieCreditsData,
+  PersonImagesData,
+  CompanyDetailsData,
+  CompanyMoviesData,
+  CollectionDetailsData,
+  MovieLatestData,
+  MovieNowPlayingData,
+  MoviePopularData,
+  MovieTopRatedData,
 } from "../src/types/types.gen";
 
 type ApiVersion = "v3" | "v4";
@@ -15,38 +32,158 @@ export type TMDBOptions = {
   baseURL?: string;
 };
 
+// Type-safe endpoint paths
+const Endpoints = {
+  SEARCH_MOVIE: "/search/movie",
+  SEARCH_PERSON: "/search/person",
+  MOVIE_DETAILS: "/movie/{movie_id}",
+  MOVIE_CREDITS: "/movie/{movie_id}/credits",
+  MOVIE_IMAGES: "/movie/{movie_id}/images",
+  MOVIE_KEYWORDS: "/movie/{movie_id}/keywords",
+  MOVIE_RELEASES: "/movie/{movie_id}/release_dates",
+  MOVIE_VIDEOS: "/movie/{movie_id}/videos",
+  MOVIE_TRANSLATIONS: "/movie/{movie_id}/translations",
+  MOVIE_ALTERNATIVE_TITLES: "/movie/{movie_id}/alternative_titles",
+  PERSON_DETAILS: "/person/{person_id}",
+  PERSON_MOVIE_CREDITS: "/person/{person_id}/movie_credits",
+  PERSON_IMAGES: "/person/{person_id}/images",
+  COMPANY_DETAILS: "/company/{company_id}",
+  COMPANY_MOVIES: "/company/{company_id}/movies",
+  COLLECTION_DETAILS: "/collection/{collection_id}",
+  CONFIGURATION: "/configuration",
+  MOVIE_LATEST: "/movie/latest",
+  MOVIE_NOW_PLAYING: "/movie/now_playing",
+  MOVIE_POPULAR: "/movie/popular",
+  MOVIE_TOP_RATED: "/movie/top_rated",
+} as const;
+
 type Operations = {
-  "/search/movie": {
+  [Endpoints.SEARCH_MOVIE]: {
     get: {
       parameters: SearchMovieData;
       responses: { 200: any };
     };
   };
-  "/movie/{movie_id}": {
+  [Endpoints.MOVIE_DETAILS]: {
     get: {
       parameters: MovieDetailsData;
       responses: { 200: any };
     };
   };
-  "/configuration": {
+  [Endpoints.CONFIGURATION]: {
     get: {
       parameters: ConfigurationDetailsData;
       responses: { 200: any };
     };
   };
-  "/search/person": {
+  [Endpoints.SEARCH_PERSON]: {
     get: {
       parameters: SearchPersonData;
       responses: { 200: any };
     };
   };
-  "/tv/{series_id}": {
+  [Endpoints.PERSON_DETAILS]: {
     get: {
-      parameters: TvSeriesDetailsData;
+      parameters: PersonDetailsData;
       responses: { 200: any };
     };
   };
-  // Add more endpoints as needed
+  [Endpoints.PERSON_MOVIE_CREDITS]: {
+    get: {
+      parameters: PersonMovieCreditsData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.PERSON_IMAGES]: {
+    get: {
+      parameters: PersonImagesData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.COMPANY_DETAILS]: {
+    get: {
+      parameters: CompanyDetailsData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.COMPANY_MOVIES]: {
+    get: {
+      parameters: CompanyMoviesData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.COLLECTION_DETAILS]: {
+    get: {
+      parameters: CollectionDetailsData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_CREDITS]: {
+    get: {
+      parameters: MovieCreditsData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_IMAGES]: {
+    get: {
+      parameters: MovieImagesData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_KEYWORDS]: {
+    get: {
+      parameters: MovieKeywordsData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_RELEASES]: {
+    get: {
+      parameters: MovieReleaseDatesData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_VIDEOS]: {
+    get: {
+      parameters: MovieVideosData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_TRANSLATIONS]: {
+    get: {
+      parameters: MovieTranslationsData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_ALTERNATIVE_TITLES]: {
+    get: {
+      parameters: MovieAlternativeTitlesData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_LATEST]: {
+    get: {
+      parameters: MovieLatestData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_NOW_PLAYING]: {
+    get: {
+      parameters: MovieNowPlayingData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_POPULAR]: {
+    get: {
+      parameters: MoviePopularData;
+      responses: { 200: any };
+    };
+  };
+  [Endpoints.MOVIE_TOP_RATED]: {
+    get: {
+      parameters: MovieTopRatedData;
+      responses: { 200: any };
+    };
+  };
 };
 
 type PathsWithMethod = {
@@ -64,67 +201,56 @@ export class TMDB {
 
   person = {
     info: (person_id: number) =>
-      this.request("/person/{person_id}", "get", { person_id }),
+      this.request(Endpoints.PERSON_DETAILS, "get", { person_id }),
     credits: (person_id: number) =>
-      this.request("/person/{person_id}/movie_credits", "get", { person_id }),
+      this.request(Endpoints.PERSON_MOVIE_CREDITS, "get", { person_id }),
     images: (person_id: number) =>
-      this.request("/person/{person_id}/images", "get", { person_id }),
+      this.request(Endpoints.PERSON_IMAGES, "get", { person_id }),
   };
 
   search = {
     movie: (query: string, page?: number) =>
-      this.request("/search/movie", "get", { query, page }),
+      this.request(Endpoints.SEARCH_MOVIE, "get", { query, page }),
     person: (query: string, page?: number) =>
-      this.request("/search/person", "get", { query, page }),
+      this.request(Endpoints.SEARCH_PERSON, "get", { query, page }),
   };
 
   company = {
     info: (company_id: number) =>
-      this.request("/company/{company_id}", "get", { company_id }),
+      this.request(Endpoints.COMPANY_DETAILS, "get", { company_id }),
     movies: (company_id: number) =>
-      this.request("/company/{company_id}/movies", "get", { company_id }),
+      this.request(Endpoints.COMPANY_MOVIES, "get", { company_id }),
   };
 
   collection = {
     info: (collection_id: number) =>
-      this.request("/collection/{collection_id}", "get", { collection_id }),
+      this.request(Endpoints.COLLECTION_DETAILS, "get", { collection_id }),
   };
 
   movie = {
     info: (params: { id?: number; imdb_id?: string }) =>
-      this.request("/movie/{movie_id}", "get", {
-        movie_id: params.id,
-        language: this.language,
-      }),
+      this.request(Endpoints.MOVIE_DETAILS, "get", { movie_id: params.id, language: this.language }),
     alternativeTitles: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/alternative_titles", "get", {
-        movie_id: params.id,
-      }),
+      this.request(Endpoints.MOVIE_ALTERNATIVE_TITLES, "get", { movie_id: params.id }),
     credits: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/credits", "get", { movie_id: params.id }),
+      this.request(Endpoints.MOVIE_CREDITS, "get", { movie_id: params.id }),
     images: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/images", "get", { movie_id: params.id }),
+      this.request(Endpoints.MOVIE_IMAGES, "get", { movie_id: params.id }),
     keywords: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/keywords", "get", {
-        movie_id: params.id,
-      }),
+      this.request(Endpoints.MOVIE_KEYWORDS, "get", { movie_id: params.id }),
     releases: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/release_dates", "get", {
-        movie_id: params.id,
-      }),
+      this.request(Endpoints.MOVIE_RELEASES, "get", { movie_id: params.id }),
     videos: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/videos", "get", { movie_id: params.id }),
+      this.request(Endpoints.MOVIE_VIDEOS, "get", { movie_id: params.id }),
     translations: (params: { id: number }) =>
-      this.request("/movie/{movie_id}/translations", "get", {
-        movie_id: params.id,
-      }),
+      this.request(Endpoints.MOVIE_TRANSLATIONS, "get", { movie_id: params.id }),
   };
 
   misc = {
-    latest: () => this.request("/movie/latest", "get"),
-    nowPlaying: () => this.request("/movie/now_playing", "get"),
-    popular: () => this.request("/movie/popular", "get"),
-    topRated: () => this.request("/movie/top_rated", "get"),
+    latest: () => this.request(Endpoints.MOVIE_LATEST, "get"),
+    nowPlaying: () => this.request(Endpoints.MOVIE_NOW_PLAYING, "get"),
+    popular: () => this.request(Endpoints.MOVIE_POPULAR, "get"),
+    topRated: () => this.request(Endpoints.MOVIE_TOP_RATED, "get"),
   };
 
   constructor(options: TMDBOptions = {}) {
@@ -196,7 +322,7 @@ export class TMDB {
   }
 
   configuration() {
-    return this.request("/configuration", "get");
+    return this.request(Endpoints.CONFIGURATION, "get");
   }
 }
 
